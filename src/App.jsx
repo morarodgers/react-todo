312e8428b02ffb7929c8db7f53e0c00243eab729
morarodgers <morarodgers@gmail.com>
2024-06-16 01:21:54 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-const welcome = {
+/* const welcome = {
   greeting: "Hello",
   title: "Rodgers Mora Onchanah",
 };
@@ -10,6 +10,21 @@ function App() {
         {welcome.greeting} {welcome.title}
       </h1>
 
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App; */
+
+function getTitle(title) {
+  return title;
+}
+function App() {
+  return (
+    <div>
+      <h1>Hello {getTitle("React")}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
